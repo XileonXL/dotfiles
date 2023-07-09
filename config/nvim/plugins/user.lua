806598e47d6a6c1dac215e0c134447233040f646
xfear <joseluis.alfaix@gmail.com>
2023-07-09 13:50:14 +0200

    Adds latex support

--- config/nvim/plugins/user.lua
+++ config/nvim/plugins/user.lua
@@ -10,6 +10,20 @@ return {
     end,
   },
   "tpope/vim-fugitive",
+  {
+    'lervag/vimtex',
+    init = function ()
+      vim.g.vimtex_enabled = true
+      vim.g.vimtex_compiler_latexmk = {
+        executable = 'latexmk',
+        options = {'-pdf'},
+      }
+      vim.g.vimtex_compiler_auto = true
+      vim.g.vimtex_view_general_options = '-reuse-instance'
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+    event = "BufWritePost *tex",
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
