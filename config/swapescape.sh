42e418e49be80f532d5cedbf5d4af4a3961b21d5
XileonXL <joseluis.alfaix@gmail.com>
2024-01-29 16:00:27 +0100

    Moves swapescape from config to customization folder

--- config/swapescape.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-current_options=$(gsettings get org.gnome.desktop.input-sources xkb-options)
-gsettings set org.gnome.desktop.input-sources xkb-options "$(echo $current_options | sed 's/\[.*\]$/['"'"'caps:swapescape'"'"']/')"
