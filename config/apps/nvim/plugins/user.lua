0fa9dcd99a80fb41e07dd60fa1faf645f0a3495a
XileonXL <joseluis.alfaix@gmail.com>
2024-05-18 17:17:58 +0200

    Adds helm syntax for nvim

--- config/apps/nvim/plugins/user.lua
+++ config/apps/nvim/plugins/user.lua
@@ -9,6 +9,10 @@ return {
     })
     end,
   },
+  {
+    "towolf/vim-helm",
+    ft = "helm"
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
