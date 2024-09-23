1dffd04dcfafe60a92200f54c3b0a003c80dc344
XileonXL <joseluis.alfaix@gmail.com>
2024-09-23 21:08:15 +0200

    feat: add linting and syntax highlight for some languages

--- config/apps/nvim/plugins/mason.lua
+++ config/apps/nvim/plugins/mason.lua
@@ -7,7 +7,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls", "dockerls", "intelephense", "jsonls", "pyright", "yamlls", "terraformls", "rust_analyzer", "helm_ls"
+        "lua_ls", "dockerls", "intelephense", "jsonls", "pyright", "yamlls", "terraformls", "rust_analyzer", "helm_ls", "golangci_lint_ls", "groovyls"
       })
     end,
   },
